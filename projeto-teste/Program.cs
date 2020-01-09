e54f0f0ce5d10ce3b76c6389146c500a6375c723
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2020-01-09 20:05:46 -0300

    Acrescentando tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -10,10 +10,12 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------------------");
             Console.WriteLine("Ola Mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("-----------------------------------------");
 
             Console.ReadLine();
         }
