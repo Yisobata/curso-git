73abbe7521b41e6f0ce27018440d20fb5fca6cc6
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2020-01-09 16:47:03 -0300

    Acrescentada a mensagem de despedida

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -13,6 +13,7 @@ namespace projeto_teste
             Console.WriteLine("Ola Mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau!");
 
             Console.ReadLine();
         }
